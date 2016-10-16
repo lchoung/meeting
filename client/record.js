2c9225d45746e00068daeb3225eff47d5b4ec1a1
Lillian Choung <lchoung@andrew.cmu.edu>
2016-10-16 09:41:09 -0400

    Basic stop/start record

--- client/record.js
+++ client/record.js
@@ -4,35 +4,38 @@ var recordRTC;
 var recordingStatus;
 var isRecording;
 
-function successStartRecordingCallback(mediaStream) {
-  var options = {
-    type: 'audio',
-  };
-  recordRTC = RecordRTC(mediaStream, options);
-  recordRTC.startRecording();
-  console.log("Started recording");
-  isRecording = true;
-}
-
-function successStopRecordingCallback(mediaStream) {
-  recordRTC.stopRecording();
-  console.log("Stopped recording.");
-  isRecording = false;
-}
-
 function errorMediaCallback(error) {
   console.log("There was an error getting the media stream.");
 }
 
 var recordModule = angular.module('recordModule', []);
-  recordModule.controller('mainController', function($scope) {
-   $scope.quantity=1;
+
+recordModule.controller('mainController', function($scope) {
+  function successStartRecordingCallback(mediaStream) {
+    var options = {
+      type: 'audio',
+    };
+    recordRTC = RecordRTC(mediaStream, options);
+    recordRTC.startRecording();
+    $scope.$apply(function() {
+      $scope.isRecording = true;
+    });
+    console.log("Started recording");
+  }
+
+  function successStopRecordingCallback(mediaStream) {
+    recordRTC.stopRecording();
+    $scope.$apply(function() {
+      $scope.isRecording = false;
+    });
+    console.log("Stopped recording.");
+  }
+
    $scope.startRecording = function() {
      var audioPromise = navigator.mediaDevices.getUserMedia({audio: true});
      audioPromise.then(successStartRecordingCallback).catch(errorMediaCallback);
    }
    $scope.stopRecording = function() {
-     console.log("Stop recording");
      var audioPromise = navigator.mediaDevices.getUserMedia({audio: true});
      audioPromise.then(successStopRecordingCallback).catch(errorMediaCallback);
    }
