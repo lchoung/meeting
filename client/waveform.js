2f8066b6e0f451d8f43d9b532664207b1497e2dc
k-kristina <vkolpakov@Vs-MacBook-Air.local>
2016-10-19 17:47:08 -0400

    Trying to do wavesurfer.js

---
+++ client/waveform.js
@@ -0,0 +1,7 @@
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+
+wavesurfer.load('https://ia801404.us.archive.org/23/items/gd79-09-04.sbd.clugston.9452.sbeok.shnf/gd79-09-04Sbdd1t03.mp3');
